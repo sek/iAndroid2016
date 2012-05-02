28d3c3f17f368fbec90e6abed4f0398c1c72cb40
vic <vic@10.0.1.37>
2012-05-01 17:22:00 -0700

    Added state machine...backing up roomba not working

--- src/org/wintrisstech/erik/iaroc/Trabant.java
+++ src/org/wintrisstech/erik/iaroc/Trabant.java
@@ -1,8 +1,10 @@
 package org.wintrisstech.erik.iaroc;
 
-import android.os.SystemClock;
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
 import org.wintrisstech.irobot.ioio.IRobotCreateInterface;
 
@@ -20,6 +22,27 @@ public class Trabant extends IRobotCreateAdapter
      */
     private int speed = 100; // The normal speed of the Trabant when going straight
     private boolean running = true;
+    private final int[][] stateTable =
+    {
+        {
+            0, 1, 2, 3
+        },
+        {
+            1, 1, 2, 3
+        },
+        {
+            2, 1, 2, 3
+        },
+        {
+            3, 1, 2, 3
+        }
+    };//State table to avoid obstacles
+    private int statePointer = 0;
+    private int presentState = 0;
+    private int howFarBacked = 200;
+    private int howFarToGoBackWhenBumped = 200;
+    private boolean nextTurnDirection = true;
+    private Random r;
 
     /**
      * Constructs a Trabant, an amazing machine!
@@ -42,24 +65,111 @@ public class Trabant extends IRobotCreateAdapter
 
     /**
      * Main method that gets the Trabant going.
-     *
      */
     public void go() throws InterruptedException
     {
-        dashboard.log("Running ...");
-        while (isRunning())
+        try
         {
-            try
+            dashboard.log("Running ...");
+            driveDirect(speed, speed);
+            howFarBacked = howFarToGoBackWhenBumped;
+            while (isRunning())
             {
-                readSensors(SENSORS_GROUP_ID6);
-                SystemClock.sleep(10000);
-                playSong(0);
-            } catch (ConnectionLostException ex)
+                try
+                {
+                    stateController();
+                } catch (Exception ex)
+                {
+                    Logger.getLogger(Trabant.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            dashboard.log("Run completed.");
+        } catch (ConnectionLostException ex)
+        {
+            Logger.getLogger(Trabant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * **************************************************************************
+     * Vic's Awesome API
+     * **************************************************************************
+     */
+    public void stateController() throws Exception
+    {
+        setStatePointer();
+        switch (stateTable[presentState][statePointer])
+        {
+            case 0:
+                presentState = 0;
+                break;
+            case 1:
+                presentState = 1;
+                backingUp("right");
+                break;
+            case 2:
+                presentState = 2;
+                backingUp("left");
+                break;
+            case 3:
+                presentState = 3;
+                backingUp("straight");
+                break;
+        }
+    }
+
+    public void setStatePointer() throws ConnectionLostException
+    {
+        readSensors(SENSORS_GROUP_ID6);
+
+        if (isBumpRight() && !isBumpLeft())//Right
+        {
+            statePointer = 1;
+        }
+        if (isBumpLeft() && !isBumpRight())//left
+        {
+            statePointer = 2;
+        }
+        if (isBumpRight() && isBumpLeft())//straight
+        {
+            statePointer = 3;
+        }
+        if (!isBumpLeft() && !isBumpRight())//none
+        {
+            statePointer = 0;
+        }
+    }
+
+    private void backingUp(String direction) throws Exception
+    {
+        if (direction.equals("right"))
+        {
+            driveDirect(-(speed), -(speed/4));
+        }
+        if (direction.equals("left"))
+        {
+            driveDirect(-(speed/4), -(speed));
+        }
+        if (direction.equals("straight"))
+        {
+            if (nextTurnDirection)
             {
-                dashboard.log("Connection Lost Exception in go()");
+                driveDirect(-(speed/4), -(speed));
+            } else
+            {
+                driveDirect(-(speed), -(speed/4));
             }
         }
-        dashboard.log("Run completed.");
+        howFarBacked += getDistance();
+        dashboard.log(howFarBacked + "/" + getDistance());
+        if (howFarBacked < 0)
+        {
+            nextTurnDirection = r.nextBoolean();
+            howFarBacked = howFarToGoBackWhenBumped;
+            driveDirect(speed, speed);
+            statePointer = 0;
+            presentState = 0;
+        }
     }
 
     /**
